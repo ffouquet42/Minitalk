6b73430180dda37a7453af001b71bb37c4e8cb5a
ffouquet42 <ffouquet42@gmail.com>
2022-11-18 07:52:48 +0100

    tests started

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,21 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 05:52:47 by fllanet           #+#    #+#             */
-/*   Updated: 2022/11/18 05:52:48 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/11/18 07:34:59 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+
+
+
+// dev
+# include <stdio.h>
+
+#endif
--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,16 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 06:32:14 by fllanet           #+#    #+#             */
-/*   Updated: 2022/11/18 06:32:15 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/11/18 07:51:55 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minitalk.h" // changer pour le Makefile
+
+int main(int argc, char **argv)
+{
+	printf("server main start\n"); // dev
+	
+	printf("server main end\n"); // dev
+	return (0);
+}
