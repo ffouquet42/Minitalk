ae21a0ea68aa46fda5ac1aecae6e8853e17eb183
ffouquet42 <ffouquet42@gmail.com>
2022-12-27 12:09:30 +0100

    start the handler function

--- srcs/server.c
+++ srcs/server.c
@@ -6,16 +6,24 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 06:32:14 by fllanet           #+#    #+#             */
-/*   Updated: 2022/11/18 07:51:55 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/12/27 12:08:04 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h" // changer pour le Makefile
 
-int main(int argc, char **argv)
+
+void	ft_get_message(int signum, siginfo_t *info, void *context)
 {
-	printf("server main start\n"); // dev
+	static unsigned char	c = 0;
+	static unsigned char	bit = 128;
+
+	(void)context; // the handler function doesn't make any use of the third argument.
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
 	
-	printf("server main end\n"); // dev
-	return (0);
+	sa.sa_sigaction = ft_get_message;
 }
