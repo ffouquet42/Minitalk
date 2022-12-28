ad41328bc7c85322209de5629012681e6fc3bbdc
ffouquet42 <ffouquet42@gmail.com>
2022-12-28 14:49:19 +0100

    get and display server pid

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,24 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/18 06:32:14 by fllanet           #+#    #+#             */
-/*   Updated: 2022/12/28 14:31:44 by fllanet          ###   ########.fr       */
+/*   Created: 2022/12/28 13:56:42 by fllanet           #+#    #+#             */
+/*   Updated: 2022/12/28 14:25:28 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h" // change for Makefile
 
-int	main(void)
+void	ft_putchar(char c)
 {
-	long				server_pid;
-	struct sigaction	data; // rename 'data'
+	write(1, &c, 1);
+}
 
-	server_pid = (long)getpid();
-	write(1, "Server PID: ", 12);
-	ft_put_longnbr(server_pid);
-	write(1, "\n", 1);
+void	ft_put_longnbr(long nb)
+{
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar('-');
+	}
+	if (nb > 9)
+	{
+		ft_put_longnbr(nb / 10);
+		ft_put_longnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
 }
